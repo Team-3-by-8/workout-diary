45eb04cb1074e0c3ab1d2d2480f4965a1669dcfa
raosk <oskari.rantamaula@gmail.com>
2023-04-12 19:03:05 +0300

    Workout date&duration input

--- app/src/main/java/fi/threebyeight/workoutdiary/ui/screens/commonElements/workoutTypeSelection.kt
+++ app/src/main/java/fi/threebyeight/workoutdiary/ui/screens/commonElements/workoutTypeSelection.kt
@@ -1,6 +1,8 @@
 package fi.threebyeight.workoutdiary.ui.screens.commonElements
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -47,10 +49,9 @@ fun WorkoutTypeSelection(
         item {
             AddNewWorkout(
                 workoutNameInput = workoutNameInput,
-                onValueChange = { workoutNameInput = it })
-
+                onValueChange = { workoutNameInput = it }
+            )
         }
-
         items(types) { type ->
             SelectionButton(
                 title = type.name,
@@ -90,6 +91,42 @@ fun AddNewWorkout(workoutNameInput: String, onValueChange: (String) -> Unit) {
 }
 
 @Composable
+fun InputBox(
+    label: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    interactionSource: MutableInteractionSource
+) {
+    TextField(
+        label = {
+            Text(
+                text = label,
+                style = MaterialTheme.typography.button,
+                color = Black,
+                modifier = Modifier
+            )
+        },
+        value = value,
+        onValueChange = onValueChange,
+        interactionSource = interactionSource,
+        textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Right),
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = White,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 10.dp)
+            .scale(scaleY = 0.9F, scaleX = 1F)
+            .height(54.dp)
+            .border(width = 2.dp, Black)
+    )
+}
+
+@Composable
 fun WorkoutConfirmation(
     chosenWorkout: String,
     setChosenWorkout: (String) -> Unit,
@@ -107,6 +144,45 @@ fun WorkoutConfirmation(
             title = chosenWorkout,
             onClick = { setChosenWorkout("") }
         )
+        if (RecordNew) {
+            Text(text = "placeholder for HR button")
+        } else {
+            var dateInput by remember { mutableStateOf("12.04") }
+            var durationInput by remember { mutableStateOf("30") }
+            //var date = dateInput  // SHOULD BE DATE
+            //var duration = durationInput.toIntOrNull() ?: 0
+            val dateInteraction = remember { MutableInteractionSource() }
+            val durationInteraction = remember { MutableInteractionSource() }
+
+            InputBox(
+                label = "Date",
+                value = dateInput,
+                onValueChange = { dateInput = it },
+                interactionSource = dateInteraction.also { interactionSource ->
+                    LaunchedEffect(interactionSource) {
+                        interactionSource.interactions.collect {
+                            if (it is PressInteraction.Release) {
+                                dateInput = ""
+                            }
+                        }
+                    }
+                }
+            )
+            InputBox(
+                label = "Duration",
+                value = durationInput,
+                onValueChange = { durationInput = it },
+                interactionSource = durationInteraction.also { interactionSource ->
+                    LaunchedEffect(interactionSource) {
+                        interactionSource.interactions.collect {
+                            if (it is PressInteraction.Release) {
+                                durationInput = ""
+                            }
+                        }
+                    }
+                }
+            )
+        }
         Spacer(modifier = Modifier.weight(1f))
         Row {
             Spacer(modifier = Modifier.weight(1.5f))
@@ -114,7 +190,7 @@ fun WorkoutConfirmation(
                 SelectionButton(title = bottomButtonTitle, onClick = { /*TODO*/ })
             }
         }
-
     }
 }
+
 
