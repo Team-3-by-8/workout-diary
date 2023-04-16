c1cc0bccfaf72bf267fbf71f5c3b6c414d31f57a
raosk <oskari.rantamaula@gmail.com>
2023-04-16 23:11:28 +0300

    Disabled main navigation for subscreens

--- app/src/main/java/fi/threebyeight/workoutdiary/ui/MainNavigation.kt
+++ app/src/main/java/fi/threebyeight/workoutdiary/ui/MainNavigation.kt
@@ -2,40 +2,56 @@ package fi.threebyeight.workoutdiary.ui
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import fi.threebyeight.workoutdiary.R
 import fi.threebyeight.workoutdiary.ui.screens.*
 import fi.threebyeight.workoutdiary.ui.theme.LightGrey
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun AppNavController(navController: NavHostController) {
+fun AppNavController(
+    navController: NavHostController,
+    setShowBottomBar: (Boolean) -> Unit
+) {
     NavHost(
         navController = navController,
         startDestination = "Workout"
     ) {
         composable(route = "Workout") {
             WorkoutScreen(navController)
+            setShowBottomBar(true)
         }
         composable(route = "Journal") {
             JournalScreen()
+            setShowBottomBar(true)
         }
         composable(route = "Plan") {
             PlanScreen()
+            setShowBottomBar(true)
         }
         composable(route = "About") {
             AboutScreen()
+            setShowBottomBar(true)
         }
         composable(route = "WorkoutNew") {
             WorkoutNewScreen(navController)
+            setShowBottomBar(false)
         }
         composable(route = "WorkoutAdd") {
             WorkoutAddScreen(navController)
+            setShowBottomBar(false)
         }
     }
 }
@@ -56,6 +72,25 @@ fun MainNavigation(items: List<TabItem>, navController: NavController) {
                 icon = { Icon(item.icon, contentDescription = null) },
                 selectedContentColor = MaterialTheme.colors.primaryVariant,
                 unselectedContentColor = Color.Gray
+            )
+        }
+    }
+}
+
+@Composable
+fun DisabledMainNavigation() {
+    BottomNavigation(
+        backgroundColor = LightGrey
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.titleTopAppBar),
+                style = MaterialTheme.typography.h2,
+                color = Color.DarkGray
             )
         }
     }
