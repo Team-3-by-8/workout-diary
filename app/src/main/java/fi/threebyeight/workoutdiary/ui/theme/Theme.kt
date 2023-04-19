d8b3679fa6cc4f30b8ce02a42bf36ae63eeb604b
raosk <oskari.rantamaula@gmail.com>
2023-04-19 23:48:08 +0300

    Dark mode disabled for now

--- app/src/main/java/fi/threebyeight/workoutdiary/ui/theme/Theme.kt
+++ app/src/main/java/fi/threebyeight/workoutdiary/ui/theme/Theme.kt
@@ -29,11 +29,12 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun WorkoutDiaryTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    val colors = LightColorPalette  // Dark mode is disabled for now
+//    val colors = if (darkTheme) {
+//        DarkColorPalette
+//    } else {
+//        LightColorPalette
+//    }
 
     MaterialTheme(
         colors = colors,
